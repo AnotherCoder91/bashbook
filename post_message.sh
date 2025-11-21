479bf33f77cbad04f7c79a0d65aaa433c483384d
18KJ18 <kjerzyk9@gmail.com>
2025-11-21 13:05:13 +0000

    Initial Commit

---
+++ post_message.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+#Check if first user exists
+if ! ls | grep -w $1 > /dev/null; then
+	#User does not exist
+	echo nok: User $1 does not exist
+	exit 1
+fi
+
+#Check if second user exists
+if ! ls | grep -w $2 > /dev/null; then
+	#User does not exist
+	echo nok: User $2 does not exist
+	exit 1
+fi
+
+#Check if sender is friends with reciever
+if ! grep $1 $2/friends.txt > /dev/null; then
+	#User is not friends with reciever
+	echo nok: User $1 is not
+echo $1: $3 >> ./$2/wall.txt
+
+#Send message to second user's wall from user 1
